2cc21a3bb06d03e583ef50246c3eab442495d9a2
Weijuer <ch_weijuer@163.com>
2018-01-30 22:33:50 +0800

    1.express4.X框架路由中获取参数值方法；

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -15,15 +15,46 @@ router.get('/', function (req, res) {
 // 匹配/api/blogList, 返回json数据
 router.post('/blogList', (req, res) => {
   console.log('BlogList Time:', Date.now());
-  console.log('params.currentPage====>' + req.params.currentPage);
-  console.log('params.pageSize====>' + req.params.pageSize);
-  res.json({ blog: blogList})
+  console.log('get请求参数对象 :', req.query);
+  console.log('post请求参数对象 :', req.body);
+
+  res.json({blog: blogList})
 });
 
 // 匹配/api/hello
 router.get('/hello', function (req, res) {
   console.log('Hello Time:', Date.now());
   res.send('Hello World!');
+});
+
+// 匹配/api/blog/:name, 获取url参数对象
+router.get('/blog/:name', function (req, res, next) {
+  console.log('url参数对象 :', req.params);
+  console.log('get请求参数对象 :', req.query);
+  console.log('post请求参数对象 :', req.body);
+  console.log('q的值为 :', req.params.name);
+
+  res.send('blog name:' + req.params.name);
+});
+
+// 匹配/api/blog?name=xiao ming， 获取get请求参数对象
+router.get('/blog', function (req, res, next) {
+  console.log('url参数对象 :', req.params);
+  console.log('get请求参数对象 :', req.query);
+  console.log('post请求参数对象 :', req.body);
+  console.log('q的值为 :', req.query.name);
+
+  res.send('blog name:' + req.query.name); // blog name:xiaoming
+});
+
+// 匹配/api/blog?name=xiao ming， 获取post请求参数对象
+router.post('/blog', function (req, res, next) {
+  console.log('url参数对象 :', req.params);
+  console.log('get请求参数对象 :', req.query);
+  console.log('post请求参数对象 :', req.body);
+  console.log('q的值为 :', req.body.name);
+
+  res.send('blog name:' + req.body.name); // blog name:xiaoming
 });
 
 module.exports = router;
