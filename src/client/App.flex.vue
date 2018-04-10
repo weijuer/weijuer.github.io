192f3cdac611ccfa4ed95390af419a0f480ead49
weijuer <ch_weijuer@163.com>
2018-04-10 10:11:11 +0800

    1.依赖版本更新； 2.部分样式修改；

---
+++ src/client/App.flex.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" :class="['wj-app-page', 'wj-layout-header-fixed', 'wj-layout-header-topbar', {'wj-page-on-scroll': $store.state.isPageScroll}]">
+    <pageHeader />
+    <pageContent />
+    <go2Top />
+  </div>
+</template>
+
+<script>
+  // 引入公共布局组件
+  import pageHeader from './components/header'
+  import pageContent from './components/content'
+  import go2Top from './components/go2Top'
+
+  export default {
+    name: 'app',
+    components: {
+      pageHeader,
+      pageContent,
+      go2Top
+    }
+  }
+</script>
+
+<style lang="less">
+  @import "static/assets/global/less/app.flex";
+  @import "static/assets/global/less/components";
+  @import "static/assets/global/less/theme/default";
+</style>
