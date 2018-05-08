f1078f5e6709273643e198db64f949c919438c5e
weijuer <ch_weijuer@163.com>
2018-05-08 17:03:19 +0800

    1.修改webpack配置； 2.flex.vue初步方案；

--- src/client/App.flex.vue
+++ src/client/App.flex.vue
@@ -1,29 +1,18 @@
 <template>
-  <div id="app" :class="['wj-app-page', 'wj-layout-header-fixed', 'wj-layout-header-topbar', {'wj-page-on-scroll': $store.state.isPageScroll}]">
-    <pageHeader />
-    <pageContent />
-    <go2Top />
+  <div id="app" :class="['wj-app-page', {'wj-page-on-scroll': $store.state.isPageScroll}]">
+    <flex-layout />
+
   </div>
 </template>
 
 <script>
   // 引入公共布局组件
-  import pageHeader from './components/header'
-  import pageContent from './components/content'
-  import go2Top from './components/go2Top'
+  import FlexLayout from './components/flex/Layout';
 
   export default {
     name: 'app-flex',
     components: {
-      pageHeader,
-      pageContent,
-      go2Top
+      FlexLayout
     }
   }
 </script>
-
-<style lang="less">
-  @import "static/assets/global/less/app.flex";
-  @import "static/assets/global/less/components";
-  @import "static/assets/global/less/theme/default";
-</style>
---
+++ src/client/components/flex/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wj-layout-page">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+
+    <pageHeader />
+    <pageContent />
+    <go2Top />
+    <router-view class="app-content-view" keep-alive transition transition-mode="out-in" />
+  </div>
+</template>
+
+<script>
+  import pageHeader from './components/header'
+  import pageContent from './components/content'
+  import go2Top from './components/go2Top'
+  export default {
+    name: 'flex-layout',
+    components: {
+      pageHeader,
+      pageContent,
+      go2Top
+    }
+  }
+</script>
+
+<style lang="less">
+  @import "@static/assets/global/less/app.flex";
+  @import "@static/assets/global/less/components";
+  @import "@static/assets/global/less/theme/default";
+</style>
+
