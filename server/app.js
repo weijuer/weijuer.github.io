bbd0fd0e7b0b133bb0394b27e35cdfbef26a5ef7
weijuer <ch_weijuer@163.com>
2018-07-12 18:13:17 +0800

    1.koa后台测试添加；

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+app.use(async ctx => {
+  ctx.body = `你请求的地址为${ctx.request.url}`;
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
