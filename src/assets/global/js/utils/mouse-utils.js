ae55c4179b7a681d683b6f73e27b0cd7e3c014b4
weijuer <ch_weijuer@163.com>
2018-09-12 18:27:39 +0800

    1.utils工具初始-获取鼠标坐标，requestAnimFrame兼容；

---
+++ src/assets/global/js/utils/mouse-utils.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// requestAnimationFrame的兼容问题
+window.requestAnimFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 6000 / 60);
+    };
+})();
+
+const mouse = {
+
+  /**
+   * 获取当前鼠标相对于屏幕的左边距与上边距的位置坐标
+   * @param event
+   * @returns {{x: *, y: *}}
+   */
+  getScreenLocate(event) {
+    let e = event || window.event;
+    return {
+      x: e.screenX,
+      y: e.screenY
+    }
+  },
+
+  /**
+   * 获取当前鼠标相对于文档的左边距与上边距的位置坐标
+   * @param event
+   * @returns {{x: *, y: *}}
+   */
+  getClientLocate(event) {
+    let e = event || window.event;
+    // 屏幕滚动的坐标
+    let scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+    let scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+
+    // 获取当前鼠标的坐标，兼容处理
+    return {
+      x: e.pageX || e.clientX + scrollX,
+      y: e.pageY || e.clientY + scrollY
+    }
+  },
+
+};
+
+export default mouse
