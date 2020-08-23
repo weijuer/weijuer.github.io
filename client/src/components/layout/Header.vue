7a02c726f80dba80a0ba2b78774802cddf739d9c
Weijuer <ch_weijuer@163.com>
2020-08-23 21:56:14 +0800

    1.更新依赖版本；2.导航样式修改

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -5,18 +5,27 @@
         <div class="app-header-logo">
           <a class="logo" href="/">
             <img class="logo-img" src="~@/assets/logo-letter.png" />
+            <!-- <span class="text-magic" data-word="W.J">W.J</span> -->
           </a>
         </div>
         <nav class="nav">
           <ul class="menu-nav app-header-nav">
             <li>
-              <router-link class="menu-link" to="/">Home</router-link>
+              <router-link class="menu-link text-effect-1" to="/" data-hover="Home">Home</router-link>
             </li>
             <li>
-              <router-link class="menu-link" to="/articles">Article</router-link>
+              <router-link
+                class="menu-link text-effect-1"
+                to="/articles"
+                data-hover="Article"
+              >Article</router-link>
             </li>
             <li>
-              <router-link class="menu-link" to="/example">Example</router-link>
+              <router-link
+                class="menu-link text-effect-1"
+                to="/example"
+                data-hover="Example"
+              >Example</router-link>
             </li>
           </ul>
         </nav>
@@ -27,9 +36,6 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
 }
 </script>
-
-<style>
-</style>
