cccd88c41e424b1d584399e00a9e39a8d027203a
Weijuer <ch_weijuer@163.com>
2020-09-04 17:11:46 +0800

    1.修改admin->Layout布局样式

--- admin/src/components/layout/Aside.vue
+++ admin/src/components/layout/Aside.vue
@@ -15,20 +15,20 @@
             version="1.1"
           >
             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <polygon points="0 0 24 0 24 24 0 24"></polygon>
+              <polygon points="0 0 24 0 24 24 0 24" />
               <path
                 d="M5.29288961,6.70710318 C4.90236532,6.31657888 4.90236532,5.68341391 5.29288961,5.29288961 C5.68341391,4.90236532 6.31657888,4.90236532 6.70710318,5.29288961 L12.7071032,11.2928896 C13.0856821,11.6714686 13.0989277,12.281055 12.7371505,12.675721 L7.23715054,18.675721 C6.86395813,19.08284 6.23139076,19.1103429 5.82427177,18.7371505 C5.41715278,18.3639581 5.38964985,17.7313908 5.76284226,17.3242718 L10.6158586,12.0300721 L5.29288961,6.70710318 Z"
                 fill="#000000"
                 fill-rule="nonzero"
                 transform="translate(8.999997, 11.999999) scale(-1, 1) translate(-8.999997, -11.999999)"
-              ></path>
+              />
               <path
                 d="M10.7071009,15.7071068 C10.3165766,16.0976311 9.68341162,16.0976311 9.29288733,15.7071068 C8.90236304,15.3165825 8.90236304,14.6834175 9.29288733,14.2928932 L15.2928873,8.29289322 C15.6714663,7.91431428 16.2810527,7.90106866 16.6757187,8.26284586 L22.6757187,13.7628459 C23.0828377,14.1360383 23.1103407,14.7686056 22.7371482,15.1757246 C22.3639558,15.5828436 21.7313885,15.6103465 21.3242695,15.2371541 L16.0300699,10.3841378 L10.7071009,15.7071068 Z"
                 fill="#000000"
                 fill-rule="nonzero"
                 opacity="0.3"
                 transform="translate(15.999997, 11.999999) scale(-1, 1) rotate(-270.000000) translate(-15.999997, -11.999999)"
-              ></path>
+              />
             </g>
           </svg>
         </i>
@@ -58,49 +58,51 @@ var MenuData = {
       children: [
         {
           name: 'child folder',
-          children: [{ name: 'hello' }, { name: 'wat' }]
+          children: [{ name: 'hello' }, { name: 'wat' }],
         },
         { name: 'hello' },
         { name: 'wat' },
         {
           name: 'child folder',
-          children: [{ name: 'hello' }, { name: 'wat' }]
-        }
-      ]
-    }
-  ]
+          children: [{ name: 'hello' }, { name: 'wat' }],
+        },
+      ],
+    },
+  ],
 }
 
 export default {
   name: 'Aside',
   components: {
     [Menu.name]: Menu,
-    [MenuItem.name]: MenuItem
+    [MenuItem.name]: MenuItem,
   },
   setup() {
     const menuData = ref(MenuData)
 
     return { menuData }
-  }
+  },
 }
 </script>
 
 <style lang="stylus">
-.aside
+.aside {
   width: 265px
-  display flex
-  flex 0 0 auto
-  flex-direction column
-  background-color: #1e1e2d;
+  display: flex
+  flex: 0 0 auto
+  flex-direction: column
+  background-color: #1e1e2d
+  transition: all 0.2s linear
 
-  &.aside-fixed
+  &.aside-fixed {
     position: fixed
     top: 0
     bottom: 0
     left: 0
     z-index: 98
+  }
 
-  .brand
+  .brand {
     display: flex
     justify-content: space-between
     align-items: center
@@ -108,49 +110,63 @@ export default {
     padding: 0 25px
     height: 65px
 
-    .brand-logo
-      > a
-        font-size: 20px;
-        color: #fff;
-        text-decoration: none;
+    .brand-logo {
+      > a {
+        font-size: 20px
+        color: #fff
+        text-decoration: none
+      }
+    }
+  }
 
-  .aside-menu
+  .aside-menu {
     position: relative
-    max-height 90vh
+    max-height: 90vh
+  }
+}
 
-.w-icon
-  display inline-block
-  width 24px
-  height 24px
-  cursor: pointer;
+.w-icon {
+  display: inline-block
+  width: 24px
+  height: 24px
+  cursor: pointer
+}
 
-.w-icon-arrow
+.w-icon-arrow {
   &:before,
-  &:after
-    content ''
-    display flex
+  &:after {
+    content: ""
+    display: flex
+  }
 
-  &:hover
-   svg
-    g [fill]
-      transition: fill 0.3s ease;
-      fill: #3699FF;
+  &:hover {
+    svg {
+      g [fill] {
+        transition: fill 0.3s ease
+        fill: #3699FF
+      }
+    }
+  }
+}
 
-@media (max-width: 991.98px)
-  .aside
-    z-index: 1001;
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: -295px;
-    width: 275px;
+@media (max-width: 991.98px) {
+  .aside {
+    z-index: 1001
+    position: fixed
+    top: 0
+    bottom: 0
+    left: -295px
+    width: 275px
+  }
+}
 
-
-@media (min-width: 992px)
-  .aside
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 98;
+@media (min-width: 992px) {
+  .aside {
+    position: fixed
+    top: 0
+    bottom: 0
+    left: 0
+    z-index: 98
+  }
+}
 </style>
--- admin/src/components/layout/Layout.vue
+++ admin/src/components/layout/Layout.vue
@@ -1,32 +1,48 @@
 <template>
-  <article class="layout">
-    <header>
+  <article class="app-layout">
+    <header class="app-header">
       <div class="container">
-        <router-link to="/">Home</router-link>| <router-link to="/about">About</router-link>|
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>|
         <router-link to="/example">Example</router-link>
       </div>
     </header>
-    <main>
+    <main class="app-main">
       <router-view />
     </main>
-    <footer>@copyright W.J</footer>
+    <footer class="app-footer">@copyright W.J</footer>
   </article>
 </template>
 
 <script>
 export default {
-  name: 'Layout'
+  name: 'Layout',
 }
 </script>
 
 <style lang="stylus" scoped>
-.layout
-  display flex
-  flex-direction column
-  min-width 0
-  flex  1 auto
+.app-layout {
+  display: flex
+  flex-direction: column
+  flex: 1 auto
+  min-width: 0
 
-@media (min-width 992px)
-  .layout
-    padding-left 265px
+  .app-header {
+    display: flex
+  }
+
+  .app-main {
+    flex: auto
+  }
+
+  .app-footer {
+    flex-shrink: 0
+  }
+}
+
+@media (min-width: 992px) {
+  .app-layout {
+    padding-left: 265px
+  }
+}
 </style>
