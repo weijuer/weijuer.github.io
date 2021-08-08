0e6652b414de0d442145c41bf5b30a162fcf55cc
Weijuer <ch_weijuer@163.com>
2021-08-08 21:42:13 +0800

    🎨 改进代码结构

--- admin/src/components/layout/menu/MenuItem.vue
+++ admin/src/components/layout/menu/MenuItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="menu-item" :class="{ active: isOpen }" :data-level="menuLevel">
-    <a href="#" class="menu-link" :style="menuItemStyle()" @click="toggle">
+    <a href="javascript:;" class="menu-link" :style="menuItemStyle()" @click="toggle">
       <i class="menu-icon">
         <w-icon name="default-menu" />
       </i>
@@ -62,63 +62,59 @@ export default {
 
 <style lang="stylus">
 .menu-item {
-  position: relative
-  margin: 0
-  padding: 0
-  display: flex
-  flex-direction: column
-  flex-grow: 1
-  transition: background-color 0.2s
+  position: relative;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  transition: background-color 0.2s;
 
   .menu-link {
-    display: flex
-    flex-grow: 1
-    align-items: stretch
-    margin: 0
-    padding: 8px 24px
-    min-height: 44px
-    position: relative
-    text-decoration: none
-    outline: none
-    cursor: pointer
-    transition: all 0.2s
+    display: flex;
+    flex-grow: 1;
+    align-items: stretch;
+    margin: 0;
+    padding: 8px 24px;
+    min-height: 44px;
+    position: relative;
+    text-decoration: none;
+    outline: none;
+    cursor: pointer;
+    transition: all 0.2s;
 
-    .menu-arrow,
-    .menu-icon {
-      display: flex
-      align-items: center
-      line-height: 0
-      color: #5c5e81
+    .menu-arrow, .menu-icon {
+      display: flex;
+      align-items: center;
+      line-height: 0;
+      color: #5c5e81;
     }
 
     .menu-arrow {
-      position: relative
+      position: relative;
 
       &:after {
-        content: "\27A4"
-        font-style: normal
-        transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1)
+        content: '\27A4';
+        font-style: normal;
+        transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
       }
     }
 
     .menu-text {
-      display: flex
-      align-items: center
-      flex-grow: 1
-      padding: 0
-      color: #a2a3b7
+      display: flex;
+      align-items: center;
+      flex-grow: 1;
+      padding: 0;
+      color: #a2a3b7;
     }
   }
 
-  &.active,
-  &:hover {
+  &.active, &:hover {
     >.menu-link {
-      background-color: #1b1b28
+      background-color: #1b1b28;
 
-      .menu-arrow,
-      .menu-icon,
-      .menu-text {
-        color: #3699FF
+      .menu-arrow, .menu-icon, .menu-text {
+        color: #3699FF;
       }
     }
   }
@@ -127,26 +123,26 @@ export default {
     >.menu-link {
       .menu-arrow {
         &:after {
-          transform: rotate(90deg)
+          transform: rotate(90deg);
         }
       }
     }
   }
 
   >.menu-submenu {
-    padding: 0
-    display: none
-    transition: all 0.3s linear
+    padding: 0;
+    display: none;
+    transition: all 0.3s linear;
 
     >.menu-item {
       >.menu-link {
-        padding-left: 40px
+        padding-left: 40px;
       }
     }
 
     &.active {
-      display: block
-      transform: translateY(0%)
+      display: block;
+      transform: translateY(0%);
     }
   }
 }
